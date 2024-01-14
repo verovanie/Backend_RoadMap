fe3fe7e0e382b86e6ab9ded4215015cd3fcfe113
Firoz <firozsulphy38620@gmail.com>
2024-01-14 17:30:23 +0530

    Learning Rest API

---
+++ Rest_CRUD_DB/src/main/java/com/rest/api/cruddemo/CruddemoApplication.java
@@ -0,0 +1,13 @@
+package com.rest.api.cruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CruddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CruddemoApplication.class, args);
+	}
+
+}
